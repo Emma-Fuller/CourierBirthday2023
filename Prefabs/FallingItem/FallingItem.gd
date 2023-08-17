ba0718d8f5674be6693d8b34a2f09d35fb1f67da
Emma Fuller <emma@emmafuller.dev>
2023-08-16 22:17:55 -0600

    Swap out bad items and make player pivot at head

--- Prefabs/FallingItem/FallingItem.gd
+++ Prefabs/FallingItem/FallingItem.gd
@@ -1,22 +1,28 @@
 class_name FallingItem extends Node2D
 
 @export var speed = 80
+@export var rotation_speed = 40
 @export var acc = 200
-@export var good_sprite: Texture2D
-@export var bad_sprite: Texture2D
+
 var item_data: FallableItem
 var destroying = false
+var rotate_scale = 1
 
 func refresh_item_data():
 	$Sprite2D.texture = item_data.sprite
 	$AudioStreamPlayer2D.stream = item_data.collect_sound
 
 func _ready():
+	if randi() % 2:
+		rotate_scale = -1
+	
 	refresh_item_data()
 	
 func _physics_process(delta):
 	position.y += delta * speed
 	speed += acc * delta
+	
+	rotation += delta * rotation_speed * rotate_scale
 
 func on_pickup(_area):
 	$AudioStreamPlayer2D.play()
