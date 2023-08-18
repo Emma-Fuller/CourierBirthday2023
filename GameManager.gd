2822ead106496ce0e1da77d7044966324b25c772
Emma Fuller <emma@emmafuller.dev>
2023-08-18 17:11:45 -0600

    Fix issues

--- GameManager.gd
+++ GameManager.gd
@@ -8,21 +8,14 @@ func spawn_item():
 	var new_item = item.instantiate() as FallingItem
 
 	new_item.position.x = get_random_window_x()
-	new_item.item_data = get_random_item_data()
+	new_item.item_data = pool.pick_random()
 
 	new_item.add_to_group("items")
 	add_child(new_item)
 	
-@export var do_filter = false
-func get_random_item_data() -> FallableItem:
-	return pool.items[randi() % pool.items.size()]
-	
 func get_random_window_x() -> int:
 	var window_size = get_viewport_rect().size
 	return randi_range(0 + drop_margin, window_size.x - drop_margin)
-	
-func is_positive(i: FallableItem) -> bool:
-	return i.score_value > 0
 
 func on_game_ended():
 	$Timer.stop()
--- Items/ItemPool.gd
+++ Items/ItemPool.gd
@@ -1,3 +1,6 @@
 class_name ItemPool extends Resource
 
 @export var items: Array[FallableItem]
+
+func pick_random() -> FallableItem:
+	return items.pick_random()
