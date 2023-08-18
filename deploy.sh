cd79629ad04f6ea01ed950af95351f8faf343daf
Emma Fuller <emma@emmafuller.dev>
2023-08-18 13:38:41 -0600

    Stuff

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ rm -rf ../EXPORT.zip ../EXPORT
 # Create new assets and publish via butler
 mkdir -p ../EXPORT
 ~/bin/godot --export-release "Web" ../EXPORT/index.html
-~/bin/butler push ../EXPORT tealberrygaming/smellie-ellies-catch-em-all
+~/bin/butler push ../EXPORT tealberrygaming/smellie-ellies-catch-em-all:EXPORT
